7f768ba53370dc61620b70f3fd03c08dc8597119
Andi Doty <andi.j.doty@gmail.com>
2025-06-24 19:14:40 -0500

    Add files via upload

--- character_select_template.py
+++ character_select_template.py
@@ -28,9 +28,11 @@ class Screen(Enum):
     Represents the possible different screens: the start screen and character select screen.
     """
 
+
+
     # Task 1:
 
-    # Create values for each screen we can display.
+    # TODO: Create values for each screen we can display.
 
 
 class GameManager:
@@ -48,37 +50,67 @@ class GameManager:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
 
-        # Task 2.1:
-
-        # Define variables to represent whether the game is running or not, assign a default value.
 
 
-        # Task 5:
+        # Task 2:
 
-        # Generate the characters' data.
-        # For each character, load their image and randomly generate their stats.
+        # TODO: Define variable to represent whether the game is running or not, assign a default value.
+
+        # TODO: Define a variable representing the current screen, assign a default value.
+
+        global character_image
+        # TODO: Initialize character_image value with the first character image.
+
+        # Scale down to size.
+        character_image = pygame.transform.scale(character_image, (WIDTH / 2, HEIGHT / 2))
+
+
+
+        # Task 4 Cont.:
+
+        # TODO: Call the load_data method after you finish writing it.
+
+
+
+        # Task 5 Cont.:
+
+        # TODO: Call the set_screen method after you finish writing it.
+
+
+
+        # Task 8
+
+        # TODO: Declare the character index and whether the character is selected.
+
+
+
+        # Task 2 Cont.:
+
+        # TODO: While the game is currently running, continue to update it.
+
+        # TODO: Otherwise, quit the program.
+
+    def load_data(self):
+        """
+        Generates the character data and saves it into the array.
+        """
+
+
+
+        # Task 4
+
+        # TODO: For each character, load their image and randomly generate their stats.
 
             # Declare any needed local variables here.
 
-            # Load up the character image and add to the character data.
+            # Load up the character image and scale it.
+
+            # Append to row.
 
             # Create random values 1 - 100 for each stat.
             # str, def, dex, int, cha
 
-            # Add the data to the character data.
-
-        # The player hasn't selected a character yet...
-        # Need variables to keep track of whether a character has been selected and which one is currently being
-        # hovered over.
-
-        # Task 2.3:
-
-        # Start on the start screen.
-
-        # While the game is currently running, continue to update it.
-
-        # Otherwise, quit the program.
-
+            # Add the row to the character data.
 
     def set_screen(self, new_screen):
         """
@@ -87,11 +119,13 @@ class GameManager:
             new_screen (Screen): The new screen we want to change to.
         """
 
-        # Task 2.2:
 
-        # Set the current screen to the new screen.
 
-        # If we are showing the start screen, randomly select one of the character images to display.
+        # Task 5:
+
+        # TODO: Set the current screen to the new screen.
+
+        # TODO: If we are showing the start screen, randomly select one of the character images to display.
 
         # Remove after you've added your implementation.
         pass
@@ -100,10 +134,9 @@ class GameManager:
     def update(self):
         """Updates the program each frame."""
 
-        # Task 4:
-
         # Get the user's input and draw the screen.
-
+        self.get_input()
+        self.draw()
         # Runs at 60 FPS.
         self.clock.tick(60)
 
@@ -112,51 +145,50 @@ class GameManager:
         Processes the user's input and take actions accordingly.
         """
 
-        # Task 3:
-
         # Process each input event.
         # Keep in mind that the get method clears the event queue, so you can use it multiple times.
-
+        for event in pygame.event.get():
             # If the user closes the window, stop running.
+            if event.type == pygame.QUIT:
+                self.running = False
 
 
-            # If the user presses a key.
 
+            # Task 7
 
-                # If the user is on the start screen.
+            # TODO: If the user presses a key.
 
+                # TODO: If the user is on the start screen.
 
                     # If the user presses escape on the start screen, stop running the game.
 
                     # If the user presses any other key, go to the character select screen.
-        w
 
-                # Task 5:
-
-                # If the user is on the character select screen.
-
+                # TODO: If the user is on the character select screen.
 
                     # If the user presses the escape key.
 
-                        # If the user hasn't selected a character yet, go back to the start screen.
+                        # TODO: Task 7 - Go back to the start screen.
 
-                        # Otherwise, deselect the currently selected character.
 
-                    # If the user presses the left arrow key and they haven't selected a character,
+
+
+                        # TODO: Task 9 - If the user hasn't selected a character yet, go back to the start screen.
+
+                        # TODO: Otherwise, deselect the currently selected character.
+
+                    # TODO: Task 9 - If the user presses the left arrow key and they haven't selected a character,
                     # move to the left.
 
 
-                    # If the user presses the right arrow key and they haven't selected a character,
+                    # TODO: If the user presses the right arrow key and they haven't selected a character,
                     # move to the right.
 
 
-                    # If the user presses enter.
+                    # TODO: If the user presses enter.
 
                         # If they haven't selected a character, select it.
 
-                        # Otherwise, continue to the next screen.
-                        #else:
-                        #    pass  # TODO: Go to next screen.
 
         # Remove after you add your implementation.
         pass
@@ -166,76 +198,118 @@ class GameManager:
         Draws the screen, including the start screen and character select screen, accordingly.
         """
 
+
+
         # Task 3:
 
-        # If we are drawing the start screen.
+        # TODO: If we are drawing the start screen.
 
-            # Make a font for the title and prompt.
+            # TODO: First, fill the screen with black to hide what was previously on the screen.
 
-            # Define the text you want to display
+            # TODO: Draw the title.
+
+            # Make the font.
+
+            # Define the text you want to display.
+
+            # Render the title.
+
+            # draw it on the top middle of the screen.
+
+            # TODO: Draw the prompt.
+
+            # Follow the same steps as with the title to render the prompt.
+
+            # TODO: Draw the character image.
 
 
-            # First, fill the screen with black to hide what was previously on the screen.
 
-            # Render the title and draw it on the top middle of the screen.
+        # Task 6:
 
-            # Render the prompt and draw it on the bottom middle of the screen.
+        # TODO: If we are drawing the character select screen.
 
-            # Draw the random character image.
+            # TODO: First, fill the screen with black to hide what was previously on the screen.
 
-        # Task 5:
-
-        # If we are drawing the character select screen.
-
-            # Define the title and instruction fonts.
-
-            # Define the title we want to display.
-
-            # Define a list of control instructions:
-            # - Left / Right Arrows
-            # - Enter
-            # Escape
-            # What should each do?
-
-            # First, fill the screen with black to hide what was previously on the screen.
-
-            # Render the title and draw it on the top middle of the screen.
-
-            # For each instruction, render the instruction and draw on the screen.
-
-            # For each character draw their image and stats.
+            # TODO: For each character draw their image and stats.
 
                 # Get the character data.
 
                 # Get the character's image and stats.
 
-                # Define the stat names for rendering purposes.
+
+
+                # Task 6.1:
+
+                # TODO: Determine the size of each image.
 
                 # Resize the images so they fit on the screen.
+
+
+
+                # Task 6.2:
+
+                # TODO: Determine the position of each image.
 
                 # Set the position of the images so they are roughly the same distance from one
                 # another across the screen.
 
-                # Draw the character's image.
+                # TODO: Draw the character's image.
 
-                # Draw the selection box if needed.
 
-                    # Define the rect's shape.
 
-                    # If the character is not selected yet, draw a yellow box.
+                # Task 6.3:
 
-                    # Otherwise draw a red box.
+                # TODO: Draw the stats of each character.
 
-                # Draw the stats.
-                # Define the starting position and font of the stats.
+                # Define the stat names for rendering purposes.
 
-                # For each stat, render and display the text on screen.
+                # Create the font.
 
-        # Update the display!
+                # Define the starting Y position.
+
+                # TODO: For each stat:
+
+                    # Define the text.
+
+                    # Render the text.
+
+                    # Determine the position.
+
+                    # Draw on screen.
+
+
+
+                # Task 8 Cont.:
+
+                # TODO: If the index of this character matches the character index.
+
+                    # TODO: If the character is not selected yet, draw a yellow box.
+
+                    # TODO: Otherwise, draw a red box.
+
+
+
+            # Task 6.4:
+
+            # TODO: Define, render, and display the title text.
+
+            # TODO: Define the instructions text:
+            # - Left / Right Arrows
+            # - Enter
+            # Escape
+            # What should each do?
+
+            # Define the font.
+
+            # TODO: For each instruction, render the instruction and draw on the screen.
+
+        # TODO: Update the display!
 
         # Remove after you add your implementation.
         pass
 
-# Task 1:
 
-# Create a game manager to begin the execution of the program!
+
+# Task 2 Cont.:
+
+# TODO: Create a game manager to begin the execution of the program!
